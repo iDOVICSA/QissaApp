0fdc38c438c254eb60d3ddfb07cdeee17342ecf4
iDOVICSA <hm_kadri@esi.dz>
2020-03-20 21:37:33 +0100

    Changing Books Page UI

---
+++ lib/Book.dart
@@ -0,0 +1,13 @@
+import 'dart:core';
+
+import 'package:flutter/cupertino.dart';
+
+class Book {
+  String title;
+  String author;
+  String downloadLink;
+  String publisher;
+  String publishingYear;
+  Image bookCover;
+  String summarize;
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:qissa_app/main.dart';
+import 'package:qissa_app/BooksPage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
